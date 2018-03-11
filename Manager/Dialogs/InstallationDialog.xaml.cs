685c9e2bd7180327c1139ed94ece75b0becc15dc
Valery Klachkov <reserfodium.none@gmail.com>
2018-03-11 15:20:35 +1000

    Simple "dialog" and his style was added

--- Manager/Dialogs/InstallationDialog.xaml.cs
+++ Manager/Dialogs/InstallationDialog.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Manager
+namespace Manager.Dialogs
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для InstallationDialog.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class InstallationDialog : Window
     {
-        public MainWindow()
+        public InstallationDialog()
         {
             InitializeComponent();
-
-            new Dialogs.InstallationDialog().Show();
         }
     }
 }
